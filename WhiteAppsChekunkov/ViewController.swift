9ddd507df2906458c4b61dd20da6d0b0247b4ca7
chekunkov <maksim.chekunkov@gmail.com>
2022-12-05 20:03:56 +0300

    “initial”

---
+++ WhiteAppsChekunkov/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhiteAppsChekunkov
+//
+//  Created by Максим Чекунков on 05.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
